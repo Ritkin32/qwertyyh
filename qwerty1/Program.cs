7a919f0872a425040f14cda19c430100b43f793d
rita <AdaRitDN@mail.ru>
2023-01-12 17:13:19 +0300

    код палиндром

---
+++ qwerty1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int val1 = 11211;
+int val2 = 0;
+int t = val1;
+while (val1 != 0)
+{
+ int o = val1 % 10;
+ val1 =val1 / 10;
+ val2 = val2 * 10 + o;
+}
+Console.WriteLine(val2);
+Console.WriteLine(t == val2);
+
+
